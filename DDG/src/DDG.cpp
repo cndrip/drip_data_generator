4373c5a3e17f6ddffc485c90fc2f08d0715713bc
cndrip <435844911@qq.com>
2018-05-25 14:14:38 +0800

    更新当前目录

--- DDG/src/DDG.cpp
+++ DDG/src/DDG.cpp
@@ -1,7 +1,16 @@
+#include <stdio.h>
 #include "DDG.h"
+#include "AfCd.h"
 
 DDG::DDG(QWidget *parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
+	AfCd::cd();
+	FILE *fp = fopen("123.txt", "wb");
+
+	fwrite("hello", 1, 5, fp);
+	fclose(fp);
+	
+
 }
